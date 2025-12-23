ddcc8ca9d1c38479ab59be1fdd4615bf094c9463
김민재 <minjaekim7111@naver.com>
2025-12-23 18:02:00 +0900

    자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,32 @@
+// 1. Number Type
+let num1 = 23;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// console.log(1 * "Hello");
+
+// 2. String Type
+let myName = "김민재";
+let myLocation = "SKU";
+let introduce = myName + myLocation;
+// console.log(introduce);
+
+// 템플릿 리터럴 문법
+let introduceTest = `${myName}은 ${myLocation}에 거주합니다.`;
+// console.log(introduceTest);
+
+// 3. boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다.)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
